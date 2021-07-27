634d28c3cb58cd7336eac3c7ff474ce1644cd056
Vladimir Pedchenko <vpe@opra.nl>
2021-07-27 12:03:06 +0200

    case study from textbook is added

---
+++ case-study-two-or-seven.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(dslabs)
+data("mnist_27")
+
+mnist_27$train %>% ggplot(aes(x_1, x_2, color = y)) + geom_point()
+fit <- mnist_27$train %>%
+  mutate(y = ifelse(y==7, 1, 0)) %>%
+  lm(y ~ x_1 + x_2, data = .)
+
+library(caret)
+p_hat <- predict(fit, newdata = mnist_27$test)
+y_hat <- factor(ifelse(p_hat > 0.5, 7, 2))
+confusionMatrix(y_hat, mnist_27$test$y)$overall[["Accuracy"]]
+
+mnist_27$true_p %>% ggplot(aes(x_1, x_2, z = p, fill = p)) +
+  geom_raster() +
+  scale_fill_gradientn(colors=c("#F8766D", "white", "#00BFC4")) +
+  stat_contour(breaks=c(0.5), color="black")
